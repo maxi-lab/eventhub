fb1e420bb74f501ee950672a47df349ec0c1e36d
Santiago Natalichio Bestosini <santiagonatalichiobestosini@alu.frlp.utn.edu.ar>
2025-05-25 16:30:45 -0300

    feat(eventos): agregar modelo para eventos favoritos, con sus migraciones

--- app/models.py
+++ app/models.py
@@ -378,6 +378,16 @@ class Comment(models.Model):
         self.save()
     
 
+class FavoriteEvent(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="favorite_events")
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="favorited_by")
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'event') 
+
+    def __str__(self):
+        return f"{self.user.username} {self.event.title}"
 
 
 
