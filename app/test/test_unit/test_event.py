a14afd925b70ddab52db2eb9dd145a534a9f35f3
augustocheca <augustocheca2@gmail.com>
2025-05-27 13:19:51 -0300

    test(eventos): Agregar test de integracion para la compra de tickets y agotamiento de evento

--- app/test/test_unit/test_event.py
+++ app/test/test_unit/test_event.py
@@ -1,8 +1,7 @@
 import datetime
 from django.test import TestCase
 from django.utils import timezone
-from app.models import Event, User,Category,Venue, Ticket
-from datetime import timedelta
+from app.models import Event, User,Category,Venue
 
 class EventModelTest(TestCase):
     def setUp(self):
@@ -259,51 +258,3 @@ class EventFilterTest(TestCase):
         self.assertEqual(future_events[0], self.future_event)
 
 
-class TicketFunctionalityTests(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='12345')
-        self.venue = Venue.objects.create(
-            name="Test Venue",
-            address="123 Street",
-            city="City",
-            capacity=100,
-            contact="123456"
-        )
-        self.category = Category.objects.create(
-            name="Test Category",
-            description="Category Desc"
-        )
-        self.event = Event.objects.create(
-            title="Test Event",
-            description="Description",
-            scheduled_at=timezone.now() + timedelta(days=5),
-            organizer=self.user,
-            category=self.category,
-            venue=self.venue
-        )
-
-    def test_total_tickets_sold_initial(self):
-        self.assertEqual(self.event.total_tickets_sold(), 0)
-
-    def test_total_tickets_sold_after_purchase(self):
-        Ticket.objects.create(
-            type_ticket="GRL",
-            event=self.event,
-            user=self.user,
-            quantity=10
-        )
-        self.assertEqual(self.event.total_tickets_sold(), 10)
-
-    def test_hay_cupo_disponible_true(self):
-        from app.views import hay_cupo_disponible
-        self.assertTrue(hay_cupo_disponible(self.event, 10))
-
-    def test_hay_cupo_disponible_false(self):
-        from app.views import hay_cupo_disponible
-        Ticket.objects.create(
-            type_ticket="VIP",
-            event=self.event,
-            user=self.user,
-            quantity=100
-        )
-        self.assertFalse(hay_cupo_disponible(self.event, 1))
